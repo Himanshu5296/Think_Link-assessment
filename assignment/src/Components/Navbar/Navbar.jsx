e8863c6dbfec1897e95f03538919116b814981eb
Himanshu5296 <himanshu.singh5296@gmail.com>
2022-09-16 23:57:38 +0530

    navbar

---
+++ assignment/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div className={style.navbar_container}>
+      <div className={style.navbar_title}>CORKWIL</div>
+    </div>
+  )
+}
+
+export default Navbar
